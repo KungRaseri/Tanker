afe4156e9b35af88202556d785f0d07f2d13bda8
KungRaseri <countjoshington@gmail.com>
2025-07-19 20:01:05 -0700

    initial

---
+++ TankerMod.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+namespace Tanker
+{
+    public class TankerMod : MonoBehaviour
+    {
+        public static string ModTag = " [Tag]"; //Whilst not required, modded items should contain 'tags' at the end of their names to prevent errors in which two mods have an item of the same name.
+
+        public static void Main()
+        {
+            ModAPI.Register(
+            new Modification()
+            {
+                OriginalItem = ModAPI.FindSpawnable("Rod"),
+                NameOverride = "placeholder item" + ModTag,
+                DescriptionOverride = "It's 6 am and theres a fly in my room and I'm scared of it.",
+                CategoryOverride = ModAPI.FindCategory("Entities"),
+                ThumbnailOverride = ModAPI.LoadSprite("sprites/placeholder.png"), //For convienence, it's best to place all sound effects and sprites within the sfx and sprites folder that comes with this base.
+                AfterSpawn = (Instance) =>
+                {
+                    ModAPI.Notify("This line will run once the item is spawned.");
+                }
+            });
+        }
+    }
+}
