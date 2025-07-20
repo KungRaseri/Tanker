a89444fe0e563cdb2f64b1c456d0bd411d3a3d05
KungRaseri <countjoshington@gmail.com>
2025-07-19 21:37:41 -0700

    initial tanker setup

--- TankerMod.cs
+++ TankerMod.cs
@@ -13,16 +13,39 @@ namespace Tanker
             ModAPI.Register(
             new Modification()
             {
-                OriginalItem = ModAPI.FindSpawnable("Rod"),
-                NameOverride = "placeholder item" + ModTag,
-                DescriptionOverride = "It's 6 am and theres a fly in my room and I'm scared of it.",
+                OriginalItem = ModAPI.FindSpawnable("Android"),
+                NameOverride = "Tanker" + ModTag,
+                DescriptionOverride = "A heavily armored tank operator with enhanced durability and combat capabilities.",
                 CategoryOverride = ModAPI.FindCategory("Entities"),
-                ThumbnailOverride = ModAPI.LoadSprite("sprites/placeholder.png"), //For convienence, it's best to place all sound effects and sprites within the sfx and sprites folder that comes with this base.
+                ThumbnailOverride = ModAPI.LoadSprite("sprites/Tanker-skin.png"),
                 AfterSpawn = (Instance) =>
                 {
-                    ModAPI.Notify("This line will run once the item is spawned.");
+                    var person = Instance.GetComponent<PersonBehaviour>();
+                    if (person != null)
+                    {
+                        // Apply custom skin texture to the person
+                        SetEntityTextures(person);
+                        SetupTankerMods(person);
+
+                        ModAPI.Notify("Tanker deployed! Enhanced armor and durability active.");
+                    }
                 }
             });
+        }
+
+        private static void SetEntityTextures(PersonBehaviour person)
+        {
+
+            var skin = ModAPI.LoadTexture("Sprites/Tanker-skin.png");
+            var flesh = ModAPI.LoadTexture("Sprites/Tanker-flesh.png");
+            var bone = ModAPI.LoadTexture("Sprites/Tanker-bone.png");
+
+            person.SetBodyTextures(skin, flesh, bone, 1);
+        }
+
+        private static void SetupTankerMods(PersonBehaviour person)
+        {
+
         }
     }
 }
